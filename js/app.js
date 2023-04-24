4dec845218ce84a3fdd4c93f8e9e8ef87408786b
Slandio Soares <lizandroelis12@hotmail.com>
2023-04-24 15:16:20 +0100

    update style.css

--- js/app.js
+++ js/app.js
@@ -3,4 +3,5 @@ const listaItems = document.querySelector(".header__navigation");
 
 menuIcon.addEventListener( "click", ()=>{
     listaItems.classList.toggle("active");
+    menuIcon.classList.toggle("active");
 })
